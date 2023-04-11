e9c5b9d4bc06cb3765a934969c43b4249ac3cf58
john <jjmiyao@gmail.com>
2023-04-10 20:48:24 -0700

    Added read/write

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueFire, VueFireAuth } from 'vuefire'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app
+  .use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()],
+  });
 
 app.mount('#app')
--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -1,5 +1,42 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+import { useFirestore, useCollection } from 'vuefire';
+import { collection, addDoc } from 'firebase/firestore';
+
+const db = useFirestore();
+const shoes = useCollection(collection(db, 'shoes'));
+
+const form = reactive({
+	name: "",
+});
+
+function addShoe() {
+	addDoc( collection(db, 'shoes'), {
+		name: form.name,
+	});
+	form.name = ""; // clear form
+}
+</script>
 
 <template>
 	<h1>Home Town Road</h1>
+
+	<ul>
+		<li v-for="shoes in shoes" :key="shoes.id">
+			{{shoes.name}}
+		</li>
+	</ul>
+
+	<form @submit.prevent="addShoe()">
+		<label for="shoe-name">Shoe Name</label>
+		<input id="shoe-name" type="text" v-model="form.name" />
+		<button type="submit">Add</button>
+	</form>
 </template>
+
+<style>
+	form {
+		display: grid;
+		grid-gap: 6px;
+	}
+</style>
