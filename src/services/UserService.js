047bbc225168e45ea51469bdcc6da03e564e10be
john <jjmiyao@gmail.com>
2023-04-16 16:36:07 -0700

    Add sign up, sign in and sign out

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,9 +1,20 @@
 import { defineStore } from 'pinia';
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { 
+	getAuth, 
+	createUserWithEmailAndPassword, 
+	signOut as  fbSignOut, 
+	signInWithEmailAndPassword, 
+} from 'firebase/auth';
+
+import { useCurrentUser } from 'vuefire';
 
 export const useUserService = defineStore('user', function() {
 	const auth = getAuth();
+
+	const current = useCurrentUser();
+	
+	console.log('current', current.email);
 
 	function signUp(email, password) {
 		createUserWithEmailAndPassword(auth, email, password)
@@ -16,13 +27,30 @@ export const useUserService = defineStore('user', function() {
 			});
 	}
 
-	function signIn() {}
+	function signIn(email, password) {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				console.log('user.signIn');
+			})
+			.catch((error) => {
+		   	console.log(error.code, error.message);
+		  	});
+	}
 
-	function signOut() {}
+	function signOut() {
+		fbSignOut(auth)
+			.then(() => {
+				
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
 	return {
 		signUp,
 		signIn,
 		signOut,
+		current,
 	};
 });
--- src/views/pages/SignInPage.vue
+++ src/views/pages/SignInPage.vue
@@ -1,47 +1,49 @@
 <script setup>
 	import { useUserService } from '@/services/UserService';
-	import { useFirebaseAuth } from 'vuefire';
+	import { useFirebaseAuth, useCurrentUser } from 'vuefire';
+	import { reactive, ref } from "vue";
 
 	const user = useUserService();
 
 	const auth = useFirebaseAuth();
 
-	// const form = reactive({
-	// 	username: '',
-	// 	password: '',
-	// });
+	const form = reactive({
+		username: '',
+		password: '',
+	});
 </script>
 
 <template>
 	<h1>Sign In Page</h1>
-	<button type="button" >Sign Out</button>
+	<button type="button" @click="user.signOut()">Sign Out</button>
 
-	<h3>{{ auth }}</h3>
+	{{ user.current }}
 
-	<form >
+	<form @submit.prevent="user.signUp(form.username, form.password)">
 		<h2>Sign Up</h2>
 		<div class="form-field">
 			<label for="email1">Email</label>
-			<input id="email1" type="text" />
+			<input id="email1" type="text" v-model="form.username"/>
 
 			<label for="password1">Password</label>
-			<input id="password1" type="password" />
+			<input id="password1" type="password" v-model="form.password"/>
+			<p class="quiet-voice">Min 6 characters</p>
 		</div>
 
 		<button type="submit">Sign Up</button>
 	</form>
 
-	<form>
+	<form @submit.prevent="user.signIn(form.username, form.password)">
 		<h2>Sign In</h2>
 		<div class="form-field">
 			<label for="email2">Email</label>
-			<input id="email2" type="text" />
+			<input id="email2" type="text" v-model="form.username"/>
 
 			<label for="password2">Password</label>
-			<input id="password2" type="password" />
+			<input id="password2" type="password" v-model="form.password"/>
 		</div>
 
-		<button type="submit">Sign Up</button>
+		<button type="submit">Sign In</button>
 	</form>
 
 </template>
